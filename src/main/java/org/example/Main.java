7edf21eb67901252d14f5824d68528b74defff04
Alassane GUEYE <84050177+alassanegu@users.noreply.github.com>
2022-11-18 10:04:16 +0100

    mon premier commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
-package org.example;public class Main {
+package org.example;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
+public class Main {
     public static void main(String[] args) {
+        Graph graph = new SingleGraph("TP3 - Graphe");
         System.out.println("Hello world!");
     }
 }
